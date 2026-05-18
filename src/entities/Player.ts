568cd5b8db057001320bd452fa76f7537947a745
hinata <hinata082121@gmail.com>
2026-05-19 01:23:43 +0900

    Redesign player as gakuran yankee character

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -9,7 +9,13 @@ export class Player extends Phaser.GameObjects.Container {
   private uniform: Phaser.GameObjects.Rectangle;
   private pants: Phaser.GameObjects.Rectangle;
   private hair: Phaser.GameObjects.Rectangle;
+  private mohawk: Phaser.GameObjects.Rectangle;
+  private sideBurn: Phaser.GameObjects.Rectangle;
   private bag: Phaser.GameObjects.Rectangle;
+  private collar: Phaser.GameObjects.Triangle;
+  private buttonA: Phaser.GameObjects.Rectangle;
+  private buttonB: Phaser.GameObjects.Rectangle;
+  private coatHem: Phaser.GameObjects.Rectangle;
   private frontArm: Phaser.GameObjects.Rectangle;
   private backArm: Phaser.GameObjects.Rectangle;
   private frontLeg: Phaser.GameObjects.Rectangle;
@@ -27,25 +33,55 @@ export class Player extends Phaser.GameObjects.Container {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y);
 
-    this.shadow = scene.add.ellipse(0, 20, 34, 12, 0x000000, 0.25);
+    this.shadow = scene.add.ellipse(0, 22, 38, 13, 0x000000, 0.28);
     this.dashDust = scene.add.circle(-18, 23, 12, THEME.colors.uiBorder, 0.35).setScale(1.2, 0.42).setVisible(false);
     this.leftStep = scene.add.ellipse(-8, 24, 7, 16, THEME.colors.uiBorder, 0.45).setVisible(false);
     this.rightStep = scene.add.ellipse(8, 24, 7, 16, THEME.colors.uiBorder, 0.45).setVisible(false);
     this.speedLineA = scene.add.rectangle(-22, 6, 18, 3, THEME.colors.uiBorder, 0.45).setVisible(false);
     this.speedLineB = scene.add.rectangle(-25, 14, 13, 3, THEME.colors.uiBorder, 0.32).setVisible(false);
-    this.backLeg = scene.add.rectangle(-5, 21, 7, 16, 0x1e3a8a, 1).setStrokeStyle(1, 0x102a68, 0.9);
-    this.frontLeg = scene.add.rectangle(7, 21, 7, 16, 0x1e3a8a, 1).setStrokeStyle(1, 0x102a68, 0.9);
-    this.bag = scene.add.rectangle(-12, 5, 9, 18, THEME.colors.playerBag, 1).setStrokeStyle(1, 0x7c2d12, 0.65);
-    this.backArm = scene.add.rectangle(-9, 6, 6, 20, THEME.colors.playerFace, 1).setStrokeStyle(1, 0x5c321d, 0.75);
-    this.uniform = scene.add.rectangle(0, 3, 21, 22, THEME.colors.playerUniform, 1).setStrokeStyle(2, 0x102a68, 1);
-    this.pants = scene.add.rectangle(0, 15, 19, 9, 0x1e3a8a, 1).setStrokeStyle(1, 0x102a68, 0.9);
-    this.frontArm = scene.add.rectangle(9, 6, 6, 20, THEME.colors.playerFace, 1).setStrokeStyle(1, 0x5c321d, 0.75);
-    this.head = scene.add.rectangle(5, -14, 18, 16, THEME.colors.playerFace, 1).setStrokeStyle(2, 0x5c321d, 0.9);
-    this.hair = scene.add.rectangle(3, -22, 20, 7, THEME.colors.playerHair, 1);
-    const nose = scene.add.rectangle(15, -12, 4, 5, THEME.colors.playerFace, 1).setStrokeStyle(1, 0x5c321d, 0.65);
+    this.backLeg = scene.add.rectangle(-6, 23, 8, 18, THEME.colors.playerPants, 1).setStrokeStyle(2, 0x050505, 0.9);
+    this.frontLeg = scene.add.rectangle(7, 23, 8, 18, THEME.colors.playerPants, 1).setStrokeStyle(2, 0x050505, 0.9);
+    this.bag = scene.add.rectangle(-13, 10, 7, 18, THEME.colors.playerBag, 1).setStrokeStyle(1, 0x050505, 0.75);
+    this.backArm = scene.add.rectangle(-11, 7, 7, 23, THEME.colors.playerUniform, 1).setStrokeStyle(2, 0x050505, 0.9);
+    this.uniform = scene.add.rectangle(0, 4, 25, 28, THEME.colors.playerUniform, 1).setStrokeStyle(3, 0xf8fafc, 0.72);
+    this.collar = scene.add.triangle(2, -7, -10, -8, 10, -8, 2, 5, THEME.colors.playerTrim, 0.92).setStrokeStyle(1, 0x5c4a32, 0.65);
+    this.buttonA = scene.add.rectangle(8, 0, 3, 3, THEME.colors.playerTrim, 1);
+    this.buttonB = scene.add.rectangle(8, 8, 3, 3, THEME.colors.playerTrim, 1);
+    this.coatHem = scene.add.rectangle(0, 18, 27, 6, THEME.colors.playerUniform, 1).setStrokeStyle(2, 0x050505, 0.8);
+    this.pants = scene.add.rectangle(0, 18, 21, 10, THEME.colors.playerPants, 1).setStrokeStyle(1, 0x050505, 0.9);
+    this.frontArm = scene.add.rectangle(12, 7, 7, 23, THEME.colors.playerUniform, 1).setStrokeStyle(2, 0x050505, 0.9);
+    this.head = scene.add.rectangle(6, -17, 20, 17, THEME.colors.playerFace, 1).setStrokeStyle(2, 0x5c321d, 0.9);
+    this.sideBurn = scene.add.rectangle(-2, -16, 6, 16, THEME.colors.playerHair, 1);
+    this.hair = scene.add.rectangle(4, -27, 19, 8, THEME.colors.playerHair, 1).setStrokeStyle(1, 0x050505, 1);
+    this.mohawk = scene.add.rectangle(3, -35, 9, 16, THEME.colors.playerHair, 1).setStrokeStyle(1, 0x050505, 1);
+    const nose = scene.add.rectangle(17, -15, 5, 5, THEME.colors.playerFace, 1).setStrokeStyle(1, 0x5c321d, 0.65);
     const eye = scene.add.rectangle(10, -15, 3, 3, 0x15101d, 1);
 
-    this.add([this.shadow, this.dashDust, this.leftStep, this.rightStep, this.speedLineA, this.speedLineB, this.backLeg, this.frontLeg, this.bag, this.backArm, this.uniform, this.pants, this.frontArm, this.head, this.hair, nose, eye]);
+    this.add([
+      this.shadow,
+      this.dashDust,
+      this.leftStep,
+      this.rightStep,
+      this.speedLineA,
+      this.speedLineB,
+      this.backLeg,
+      this.frontLeg,
+      this.bag,
+      this.backArm,
+      this.uniform,
+      this.collar,
+      this.buttonA,
+      this.buttonB,
+      this.coatHem,
+      this.pants,
+      this.frontArm,
+      this.head,
+      this.sideBurn,
+      this.hair,
+      this.mohawk,
+      nose,
+      eye
+    ]);
     this.setDepth(40);
     scene.add.existing(this);
   }
@@ -123,20 +159,25 @@ export class Player extends Phaser.GameObjects.Container {
 
     this.uniform.setRotation(lean);
     this.pants.setRotation(lean);
+    this.collar.setRotation(lean);
+    this.coatHem.setRotation(lean);
     this.head.setRotation(lean * 0.5);
     this.hair.setRotation(lean * 0.5);
-    this.bag.setPosition(state === 'dash' ? -16 : -12, state === 'dash' ? 7 : 5);
-    this.bag.setRotation(state === 'dash' ? -0.12 : 0);
-    this.shadow.setScale(state === 'dash' ? 1.22 : 1, state === 'dash' ? 0.9 : 1);
+    this.mohawk.setRotation(lean * 0.35);
+    this.sideBurn.setRotation(lean * 0.45);
+    this.bag.setPosition(state === 'dash' ? -16 : -13, state === 'dash' ? 12 : 10);
+    this.bag.setRotation(state === 'dash' ? -0.1 : state === 'walk' ? phase * 0.04 : 0);
+    this.shadow.setScale(state === 'dash' ? 1.28 : state === 'walk' ? 1.08 : 1, state === 'dash' ? 0.86 : 1);
 
     this.frontLeg.setPosition(7 + phase * stride * 0.45, 21);
     this.backLeg.setPosition(-5 - phase * stride * 0.45, 21);
     this.frontLeg.setRotation(phase * (state === 'dash' ? 0.32 : 0.18));
     this.backLeg.setRotation(-phase * (state === 'dash' ? 0.32 : 0.18));
-    this.frontArm.setPosition(9 - phase * arm * 0.45, 6);
-    this.backArm.setPosition(-9 + phase * arm * 0.45, 6);
-    this.frontArm.setRotation(-phase * (state === 'dash' ? 0.45 : 0.22));
-    this.backArm.setRotation(phase * (state === 'dash' ? 0.45 : 0.22));
+    this.frontArm.setPosition(12 - phase * arm * 0.45, 7);
+    this.backArm.setPosition(-11 + phase * arm * 0.45, 7);
+    this.frontArm.setRotation(-phase * (state === 'dash' ? 0.48 : 0.24));
+    this.backArm.setRotation(phase * (state === 'dash' ? 0.48 : 0.24));
+    this.coatHem.setY(state === 'dash' ? 19 + phase * 0.7 : 18);
   }
 
   getPositionVector(): Phaser.Math.Vector2 {
